e07a85203f4db7eff65ec8799abe65928364c1a7
Aaron Weigl <aaron.weigl@hotmail.com>
2018-05-22 15:04:41 +0200

    server added

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
+app.use(express.static(`${__dirname}/public`))
+
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/main.html`)
+    res.sendFile(`${__dirname}/public/main.html`)
 })
 
 app.listen(port, () =>
