726f360bfb63bec66e337b322c06f3b114222410
Parinaz Roghany <parinazroghany@yahoo.de>
2018-05-25 15:48:03 +0200

    add content and style

--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
-console.log("hello world")
 
 function scrollToAnchor(aid) {
     var aTag = $("a[name='" + aid + "']")
@@ -8,7 +7,32 @@ function scrollToAnchor(aid) {
 }
 
 
-$('#link').click(function () {
-    console.log("HEllo");
-    scrollToAnchor('super');
+$('#resume').click(function () {
+    scrollToAnchor('resume');
 })
+
+$('#projects').click(function () {
+    scrollToAnchor('projects');
+})
+
+$('#contact').click(function () {
+    scrollToAnchor('contact');
+})
+
+$('#skills').click(function () {
+    scrollToAnchor('skills');
+})
+
+
+
+var menu = $(".myDIV");
+var menuItems = $(".menuItem");
+
+  menuItems.on("click", function() {
+    for (var i = 0; i < menuItems.length; i++) {
+    var currentItem = $(".active");
+    currentItem.removeClass('active');
+    $(this).addClass('active');
+
+  }
+});
