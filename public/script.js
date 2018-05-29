5975d6f9e174e3495c38a27906383787e44db637
Parinaz Roghany <parinazroghany@yahoo.de>
2018-05-29 09:42:22 +0200

    add content, projects and styling

--- public/script.js
+++ public/script.js
@@ -36,3 +36,38 @@ var menuItems = $(".menuItem");
 
   }
 });
+
+// var center = $(window).scrollTop() + $(window).height() / 2;
+var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+var top = (height / 2);
+
+var demo = $("video");
+$("#spaetifinder").on("click", () => {
+  console.log("scrollTop:",$(window).scrollTop());
+  console.log("height", $(window).height());
+  console.log($(window).innerHeight);
+  console.log("offset TOP",$("#spaetifinder").offset().top);
+
+  console.log("In iframe showing function");
+    $(".video").css({display: "flex", top: $(window).height()/2 - $(this).height()/2});
+    $("video").attr("src", "./demos/spaetifinder_demo.mp4")
+    // demo.get().play();
+
+})
+
+$("#petition").on("click", () => {
+  console.log("In iframe showing function");
+  console.log("scrollTop:",$(window).scrollTop());
+  console.log("height", $(window).height());
+  console.log("offset TOP", $("#petition").offset().top);
+  $(".video").css({display: "flex", top: $(window).height() - $(this).height()/2});
+      $("video").attr("src", "./demos/petition_demo.mp4")
+    // demo.get().play();
+
+})
+
+$(".video").on("click", () => {
+console.log("closing modal");
+      $(".video").css({display: "none"});
+      // demo.get().pause();
+})
